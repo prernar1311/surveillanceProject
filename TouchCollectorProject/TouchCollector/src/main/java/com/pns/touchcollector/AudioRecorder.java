df788d23a6dfcf61de3b63bc5c9d73b35e525cc7
Nicolas Crowell <nicolas.crowell@gmail.com>
2014-01-09 00:21:56 -0500

    Add AudioRecorder class, with example code moved to separate file.

--- TouchCollectorProject/TouchCollector/src/main/java/com/pns/touchcollector/AudioRecorder.java
+++ TouchCollectorProject/TouchCollector/src/main/java/com/pns/touchcollector/AudioRecorder.java
@@ -10,9 +10,7 @@
  */
 package com.pns.touchcollector;
 
-import android.app.Activity;
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,15 +23,13 @@ import android.widget.LinearLayout;
 import java.io.IOException;
 
 
-public class AudioRecorder extends Activity {
+public class AudioRecorder  {
     private static final String LOG_TAG = "AudioRecorder";
-    private static String mFileName = null;
+    private static final int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+    private String mFileName = null;
 
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton mPlayButton = null;
-    private MediaPlayer mPlayer = null;
+    private boolean recording;
+    private boolean startedRecording;
 
     private void onRecord(boolean start) {
         if (start) {
@@ -41,30 +37,6 @@ public class AudioRecorder extends Activity {
         } else {
             stopRecording();
         }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
     }
 
     private void startRecording() {
@@ -77,98 +49,62 @@ public class AudioRecorder extends Activity {
         try {
             mRecorder.prepare();
         } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
+            Log.e(LOG_TAG, "prepare() failed", e);
         }
 
         mRecorder.start();
+        startedRecording = true;
+        recordingNow = true;
     }
 
     private void stopRecording() {
         mRecorder.stop();
         mRecorder.release();
         mRecorder = null;
+        recordingNow = false;
     }
 
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
+/*
+ *    class RecordButton extends Button {
+ *        boolean mStartRecording = true;
+ *
+ *        OnClickListener clicker = new OnClickListener() {
+ *            public void onClick(View v) {
+ *                onRecord(mStartRecording);
+ *                if (mStartRecording) {
+ *                    setText("Stop recording");
+ *                } else {
+ *                    setText("Start recording");
+ *                }
+ *                mStartRecording = !mStartRecording;
+ *            }
+ *        };
+ *
+ *        public RecordButton(Context ctx) {
+ *            super(ctx);
+ *            setText("Start recording");
+ *            setOnClickListener(clicker);
+ *        }
+ *    }
+ */
 
     public AudioRecorder() {
+        final long lNow = System.currentTimeMillis() / 1000L;
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecorder.3gp";
+        mFileName += "/microphone_recording-" + AUDIO_SOURCE + "-" + lNow + ".3gp";
     }
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
+    public String getRecording() {
+        if (recordingNow) throw new StillRecordingException();
+        return mFileName;
     }
 
-    @Override
     public void onPause() {
-        super.onPause();
         if (mRecorder != null) {
             mRecorder.release();
             mRecorder = null;
         }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
     }
+
+    public class StillRecordingException extends Exception {}
 }
