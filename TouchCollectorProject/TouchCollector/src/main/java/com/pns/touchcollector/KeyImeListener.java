601c37915e2b3e0f6b6c1be069213ae49d86a4ab
Nicolas Crowell <nicolas.crowell@gmail.com>
2014-01-10 05:37:52 -0500

    static interface -> interface

--- TouchCollectorProject/TouchCollector/src/main/java/com/pns/touchcollector/KeyImeListener.java
+++ TouchCollectorProject/TouchCollector/src/main/java/com/pns/touchcollector/KeyImeListener.java
@@ -6,6 +6,6 @@ import android.view.KeyEvent;
  * Created by nicolascrowell on 2014/1/10.
  */
 
-public static interface KeyImeListener {
+public interface KeyImeListener {
     public void onKeyIme(int keyCode, KeyEvent event);
 }
